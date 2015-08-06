2173dbc33aacbc97efc180a9210481f39d709f13
Mikhail Plekhanov <m.plekhanov@netris.ru>
2015-08-06 15:13:47 +0300

    Boost asio ex. 4.

--- boost/asio/tuttimer.cpp
+++ boost/asio/tuttimer.cpp
@@ -3,29 +3,39 @@
 #include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-void print(const boost::system::error_code& /*e*/,
-        boost::asio::deadline_timer* t, int* count)
-{
-    if(*count < 5)
+class printer {
+public:
+    printer(boost::asio::io_service& io) :
+        timer_(io, boost::posix_time::seconds(1)),
+        count_(0)
     {
-        std::cout << *count << std::endl;
-        ++(*count);
-        
-        t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
-        t->async_wait(boost::bind(print,
-                    boost::asio::placeholders::error,t,count));
+        timer_.async_wait(boost::bind(&printer::print, this));
     }
-}
+
+    ~printer() {
+        std::cout << "The final count is " << count_ << std::endl;
+    }
+
+    void print() {
+        if(count_ < 5)
+        {
+            std::cout << count_ << std::endl;
+            ++count_;
+            
+            timer_.expires_at(timer_.expires_at() + boost::posix_time::seconds(1));
+            timer_.async_wait(boost::bind(&printer::print, this));
+        }
+    }
+    
+private:
+    boost::asio::deadline_timer timer_;
+    int count_;
+};
 
 int main() {
     boost::asio::io_service io;
-    int count = 0;
-    boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
-    t.async_wait(boost::bind(print,
-                boost::asio::placeholders::error,&t,&count));
+    printer p(io);
     io.run();
-
-    std::cout << "The final count is " << count << std::endl;
 
     return 0;
 }
