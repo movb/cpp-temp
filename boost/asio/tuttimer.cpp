ddc45cfb5b4c550a9ce58156c86184cb0f87f03e
Mikhail Plekhanov <m.plekhanov@netris.ru>
2015-08-06 14:50:06 +0300

    Boost asio ex. 2.

--- boost/asio/tuttimer.cpp
+++ boost/asio/tuttimer.cpp
@@ -2,11 +2,16 @@
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
+void print(const boost::system::error_code& /*e*/)
+{
+    std::cout << "Hello, boost!";
+}
+
 int main() {
     boost::asio::io_service io;
     boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
-    t.wait();
-    std::cout << "Hello, boost!";
+    t.async_wait(&print);
+    io.run();
 
     return 0;
 }
