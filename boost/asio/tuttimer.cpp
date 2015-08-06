39f3980fd4c22b7349b37bcb849cdc0392425b09
Mikhail Plekhanov <m.plekhanov@netris.ru>
2015-08-06 15:00:55 +0300

    Boost asio ex. 3.

--- boost/asio/tuttimer.cpp
+++ boost/asio/tuttimer.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
 #include <boost/asio.hpp>
+#include <boost/bind.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-void print(const boost::system::error_code& /*e*/)
+void print(const boost::system::error_code& /*e*/,
+        boost::asio::deadline_timer* t, int* count)
 {
-    std::cout << "Hello, boost!";
+    if(*count < 5)
+    {
+        std::cout << *count << std::endl;
+        ++(*count);
+        
+        t->expires_at(t->expires_at() + boost::posix_time::seconds(1));
+        t->async_wait(boost::bind(print,
+                    boost::asio::placeholders::error,t,count));
+    }
 }
 
 int main() {
     boost::asio::io_service io;
-    boost::asio::deadline_timer t(io, boost::posix_time::seconds(5));
-    t.async_wait(&print);
+    int count = 0;
+    boost::asio::deadline_timer t(io, boost::posix_time::seconds(1));
+    t.async_wait(boost::bind(print,
+                boost::asio::placeholders::error,&t,&count));
     io.run();
+
+    std::cout << "The final count is " << count << std::endl;
 
     return 0;
 }
